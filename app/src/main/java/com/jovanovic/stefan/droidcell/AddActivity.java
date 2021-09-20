fb098ca8855a537ad3d60ef0528656f03a6b6398
nethmini99 <manthika99911@gmail.com>
2021-09-21 02:48:32 +0530

    11th commit

--- app/src/main/java/com/jovanovic/stefan/droidcell/AddActivity.java
+++ app/src/main/java/com/jovanovic/stefan/droidcell/AddActivity.java
@@ -9,17 +9,21 @@ import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
 
-    EditText title_input, author_input, pages_input;
+    EditText pid,fname,dob,address,phone,trustee;
     Button add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        setContentView(R.layout.activity_add2);
 
-        title_input = findViewById(R.id.title_input);
-        author_input = findViewById(R.id.author_input);
-        pages_input = findViewById(R.id.pages_input);
+        pid = findViewById(R.id.pid);
+        fname = findViewById(R.id.fname);
+        dob = findViewById(R.id.dob);
+        address = findViewById(R.id.address);
+        phone = findViewById(R.id.phone);
+        trustee= findViewById(R.id.trustee);
         add_button = findViewById(R.id.add_button);
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
